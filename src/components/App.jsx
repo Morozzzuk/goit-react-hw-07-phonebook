7e87eddb277e81084602bc8999b6d220422374ee
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-07-05 19:42:29 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import { Container, Section, Title } from './App.styled';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+const App = () => {
+  return (
+    <Container>
+      <Section title="Phonebook">
+        <Title>Phonebook</Title>
+        <ContactForm />
+      </Section>
+
+      <Section title="Contacts">
+        <Title>Contacts</Title>
+        <Filter />
+        <ContactList />
+      </Section>
+    </Container>
+  );
+};
+
+export default App;
